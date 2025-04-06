343094efafddcdbffb0c168774db68798fc5bce1
Trollninja228 <timur5667262005@gmail.com>
2025-04-06 12:53:10 +0500

    Proj

--- CRM_Project/Backend/crm_back/authAPI/models.py
+++ CRM_Project/Backend/crm_back/authAPI/models.py
@@ -5,7 +5,7 @@ class Profile(models.Model):
     FirstName = models.CharField(max_length=100)
     LastName = models.CharField(max_length=100)
     password = models.CharField(max_length=255)
-    mail = models.CharField(max_length=255)
+    mail = models.EmailField(max_length=255)
     phone_num = models.CharField(max_length=100)
     age = models.IntegerField()
     PhotoFileName = models.CharField(max_length=100)
