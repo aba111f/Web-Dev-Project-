e321e39f552e9f1e5b7440babb78e3e8e22bee88
Trollninja228 <timur5667262005@gmail.com>
2025-04-13 17:54:28 +0500

    tr

--- CRM_Project/Backend/crm_back/authAPI/urls.py
+++ CRM_Project/Backend/crm_back/authAPI/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('getData/', DataProfile.as_view()),
     path('Graphics/ActiveClients/',getGraphicsActiveClients.as_view()),
     path('Graphics/ActiveProjects/',getGraphicsActiveProjects.as_view()),
-    path('Graphics/QuarterlyRevenue/',getGraphicsQuarterlyRevenue.as_view()),
+    # path('Graphics/QuarterlyRevenue/',getGraphicsQuarterlyRevenue.as_view()),
     path('Graphics/TotalProfit/',getGraphicsTotalProfit.as_view())
 ]
