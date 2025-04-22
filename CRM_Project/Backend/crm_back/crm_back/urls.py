518def6cbaa0d27ddd8c1350b506bfa7cbff3685
aba111f <a_khamzin@mail.ru>
2025-04-22 15:36:45 +0500

    new

--- CRM_Project/Backend/crm_back/crm_back/urls.py
+++ CRM_Project/Backend/crm_back/crm_back/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-
+import os
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('authAPI.urls')),
@@ -26,3 +27,5 @@ urlpatterns = [
     path('refresh/', TokenRefreshView.as_view())
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
