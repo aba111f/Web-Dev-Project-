b23fb90c561ea8da958f61ce8d4e081c62fa0559
Trollninja228 <timur5667262005@gmail.com>
2025-04-22 19:10:26 +0500

    Employee_back

--- CRM_Project/Backend/crm_back/authAPI/serializers.py
+++ CRM_Project/Backend/crm_back/authAPI/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from authAPI.models import Profile
+from authAPI.models import *
 
 class SerializerProfile(serializers.ModelSerializer):
     class Meta:
@@ -31,3 +31,9 @@ class SerializerProfile(serializers.ModelSerializer):
             **validated_data
         )
         return user
+    
+class SerializerEmployee(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+        read_only_fields = ('user_id',)
